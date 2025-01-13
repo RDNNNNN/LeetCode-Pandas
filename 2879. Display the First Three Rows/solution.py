0673154c611f6d1866e7ac208981c440cbe56eef
Ma <z2715674@gmail.com>
2025-01-13 22:42:47 +0800

    update 2879. Display the First Three Rows

--- 2879. Display the First Three Rows/solution.py
+++ 2879. Display the First Three Rows/solution.py
@@ -42,33 +42,65 @@
 import pandas as pd
 
 
-# slice
-def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-    return employees[:3]
+# concat()
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2], axis=0)
 
 
-# head()
-# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-#     return employees.head(3)
+# pd.concat() 預設會沿著 row 方向合併
+# axis=0 表示垂直合
 
 
-# iloc()
-# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-#     return employees.iloc[0:3]
+# append()
+# def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+#     return df1.append(df2, ignore_index=True)
 
-# loc()
-# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-#     return employees.loc[0:2]
+# Pandas 1.4.0 開始已停用此方法。
+# append() 會直接將 df2 的資料附加到 df1 後面
+# ignore_index=Ture 表示重新索引，避免索引重複
 
 
-# query()
-# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-#     return employees.query('index < 3')
+# merge()
+# def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+#     return pd.merge(df1, df2, how='outer')
 
-# filter()
-# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-#     return employees.filter(items=range(3), axis=0)
+# 通常用於 Key 的合併，也可以用於垂直合併
+# merge() 會根據共同的欄位合併
+# how="outer" 表示取兩個 DataFrame 的 Union (聯集，將兩個資料合併並保留所有的唯一值)
 
-# take()
-# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-#     return employees.take([0, 1, 2])
+
+# join()
+# def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+#     return df1.T.join(df2.T).T
+# 通常用於橫向合併，但可以透過 T 轉置操作模擬垂直合併
+# T 為轉置操作，可以將 DataFrame 的欄和列交換位置
+# join() 會基於索引進行合併
+
+
+# combine_first()
+# def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+#     return df1.combine_first(df2)
+
+# 通常用於資料補充
+# combine_first() 用來合併兩個 DataFrame，並且會優先保留第一個 DataFrame 的值
+# combine_first() 會用 df1 的值填補 df2 的缺少值
+
+
+# reindex()
+# def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+#     combined = pd.concat([df1, df2])
+#     return combined.reset_index(drop=True)
+
+# 合併兩個 DataFrame 重新排列索引，並移除原本的索引
+
+
+# numpy
+# import numpy as np
+
+# def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+#     combined_array = np.vstack([df1.values, df2.values])
+#     return pd.DataFrame(combined_array, columns=df1.columns)
+
+# numpy 可以達到更底層的控制
+# np.vstack() 將兩個陣列垂直合併
+# 使用 pd.DataFrame() 將結果轉成 DataFrame
