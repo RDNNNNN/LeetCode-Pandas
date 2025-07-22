c3d8a7cb70183db41091f9c96a9e5e36dab97721
Ma <z2715674@gmail.com>
2025-07-22 11:01:24 +0800

    update 2882. Drop Duplicate Rows

--- 2882. Drop Duplicate Rows/solution.py
+++ 2882. Drop Duplicate Rows/solution.py
@@ -9,6 +9,7 @@
 # | email       | object |
 # +-------------+--------+
 
+### 題目
 
 # There are some duplicate rows in the DataFrame based on the email column.
 # Write a solution to remove these duplicate rows and keep only the first occurrence.
@@ -19,7 +20,6 @@
 # 結果顯示如下
 
 ### Example 範例:
-
 
 # Input:
 # +-------------+---------+---------------------+
