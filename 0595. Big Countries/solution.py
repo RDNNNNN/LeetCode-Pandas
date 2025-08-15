e2804d4ca61bf00cef48dcba1067529c135f56fe
RDNNNNN <z2715674@gmail.com>
2025-08-15 10:32:39 +0800

    update 0595. Big Countries

--- 0595. Big Countries/solution.py
+++ 0595. Big Countries/solution.py
@@ -17,16 +17,16 @@
 
 ### 題目
 # A country is big if:
-# it has an area of at least three million (i.e., 3000000 km2), or
-# it has a population of at least twenty-five million (i.e., 25000000).
+# it has an area of at least three million (i.e., `3000000 km2`), or
+# it has a population of at least twenty-five million (i.e., `25000000`).
 # Write a solution to find the name, population, and area of the big countries.
 # Return the result table in any order.
 # The result format is in the following example.
 
 ### 中文
 # 如果一個國家有以下特點，那麼這個國家就很大：
-# 其面積至少為 300 萬平方公里（即 300,000 平方公里），或
-# 它的人口至少有二千五百萬（即25000000）
+# 其面積至少為 300 萬平方公里（即 3000000 平方公里）
+# 或者它的人口至少有二千五百萬（即 25000000 ）
 # 寫出解決方案來找出大國的名稱、人口和面積
 # 以任意順序傳回結果表
 # 結果顯示如下
