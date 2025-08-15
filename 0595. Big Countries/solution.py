293efad5a1ae45b6cd8cca22d4ad79860d124a70
RDNNNNN <z2715674@gmail.com>
2025-08-15 10:29:34 +0800

    update 595. Big Countries

--- 0595. Big Countries/solution.py
+++ 0595. Big Countries/solution.py
@@ -10,15 +10,12 @@
 # | population  | int     |
 # | gdp         | bigint  |
 # +-------------+---------+
-
 # name is the primary key (column with unique values) for this table.
 # Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
-
 # 名稱是此表的主鍵（具有唯一值的欄位）
 # 表的每一行都提供有關一個國家的名稱、所屬大陸、面積、人口和 GDP 值的資訊
 
 ### 題目
-
 # A country is big if:
 # it has an area of at least three million (i.e., 3000000 km2), or
 # it has a population of at least twenty-five million (i.e., 25000000).
@@ -27,7 +24,6 @@
 # The result format is in the following example.
 
 ### 中文
-
 # 如果一個國家有以下特點，那麼這個國家就很大：
 # 其面積至少為 300 萬平方公里（即 300,000 平方公里），或
 # 它的人口至少有二千五百萬（即25000000）
@@ -36,7 +32,6 @@
 # 結果顯示如下
 
 ### Example 範例
-
 # Input:
 # World table:
 # +-------------+-----------+---------+------------+--------------+
@@ -48,7 +43,6 @@
 # | Andorra     | Europe    | 468     | 78115      | 3712000000   |
 # | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
 # +-------------+-----------+---------+------------+--------------+
-
 # Output:
 # +-------------+------------+---------+
 # | name        | population | area    |
@@ -58,7 +52,6 @@
 # +-------------+------------+---------+
 
 ### Code 程式碼
-
 import pandas as pd
 
 
