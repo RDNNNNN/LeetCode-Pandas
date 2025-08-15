547a6754d97aedc8b42cffc1049c75962b33f063
RDNNNNN <z2715674@gmail.com>
2025-08-15 10:55:21 +0800

    update 1757. Recyclable and Low Fat Products

--- 1757. Recyclable and Low Fat Products/solution.py
+++ 1757. Recyclable and Low Fat Products/solution.py
@@ -8,29 +8,24 @@
 # | low_fats    | enum    |
 # | recyclable  | enum    |
 # +-------------+---------+
-
 # product_id is the primary key (column with unique values) for this table.
 # low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
 # recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
-
 # Product_id 為表格的 primary key （具有唯一值的欄位）
 # low_fats 是一種 ENUM (類別)，其中 'Y' 表示為這個產品為低脂，'N' 則不為低脂產品
 # recyclable 是一種 ENUM (類別)，其中 'Y' 表示這個產品可回收，'N' 則不能被回收
 
 ### 題目
-
 # Write a solution to find the ids of products that are both low fat and recyclable.
 # Return the result table in any order.
 # The result format is in the following example.
 
 ### 中文
-
 # 寫出一個解決方案來尋找低脂且可回收的產品 ID
 # 以任意順序回傳結果
 # 結果顯示如下
 
 ### Example 範例
-
 # Input:
 # Products table:
 # +-------------+----------+------------+
@@ -42,7 +37,6 @@
 # | 3           | Y        | Y          |
 # | 4           | N        | N          |
 # +-------------+----------+------------+
-
 # Output:
 # +-------------+
 # | product_id  |
@@ -52,12 +46,10 @@
 # +-------------+
 
 ### Explanation 解釋
-
 # Only products 1 and 3 are both low fat and recyclable.
 # 只有產品 1 跟 3 是低脂且可以被回收的
 
 ### Code 程式碼
-
 import pandas as pd
 
 
